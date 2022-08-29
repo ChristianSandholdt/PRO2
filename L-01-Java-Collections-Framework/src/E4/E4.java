4897753344c2aed0a9097570d02937ec815c3080
ChristianSandholdt <100411100+ChristianSandholdt@users.noreply.github.com>
2022-08-29 13:01:49 +0200

    e2

--- L-01-Java-Collections-Framework/src/E4/E4.java
+++ L-01-Java-Collections-Framework/src/E4/E4.java
@@ -9,39 +9,43 @@ import java.util.*;
 public class E4 {
     public static void main(String[] args) throws FileNotFoundException {
 
+        System.out.println("Opgave A");
+
         File in = new File("L-01-Java-Collections-Framework/src/E4/text");
+        int count = 0;
         try (Scanner scanner = new Scanner(in)) {
             Set<String> words = new TreeSet<>();
-            ArrayList<String> arrWords = new ArrayList<>();
+            scanner.useDelimiter("[^A-ZÆØÅa-zæøå'-]+");
             while (scanner.hasNext()){
-                String word = scanner.next().toLowerCase();
+                String word = scanner.next();
+                count++;
                 words.add(word);
-                arrWords.add(word);
             }
 
             System.out.println("A");
-            System.out.println("Total number of words: " + arrWords.size());
+            System.out.println("Total number of words: " + count);
+            System.out.println(words);
             System.out.println("Different words: " + words.size());
         }
-
+//opgave b printer ikke alle ord med value over 1000 ud
+        System.out.println("Opgave B");
+        Map<String, Integer> map = new TreeMap<>();
         File in1 = new File("L-01-Java-Collections-Framework/src/E4/text");
         try (Scanner scanner = new Scanner(in1)) {
-            Map<String, Integer> map = new TreeMap<>();
-            int sum = 0;
+            scanner.useDelimiter("[^A-ZÆØÅa-zæøå'-]+");
             while (scanner.hasNext()){
+                int sum = 0;
                 String word = scanner.next();
-                map.put(word, sum++);
+                sum++;
+                map.put(word, sum);
         }
-//            int i = 0;
-//            for (String word : map.keySet()){
-//                i++;
-//                if (i>= 1000){
-//                    System.out.println();
-//                }
-//            }
             System.out.println(map.size());
-            System.out.println(sum);
     }
+        for(Map.Entry<String, Integer> e : map.entrySet()){
+            if(e.getValue() > 1000){
+                System.out.println(e.getKey() + " " + e.getValue());
+            }
+        }
 }
 
 }
