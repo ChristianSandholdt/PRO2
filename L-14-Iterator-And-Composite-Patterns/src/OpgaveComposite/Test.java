f629b55254ec2a0061c85bf1c6a81f0747f98768
ChristianSandholdt <100411100+ChristianSandholdt@users.noreply.github.com>
2022-10-05 14:24:08 +0200

    L-14 test

--- L-14-Iterator-And-Composite-Patterns/src/OpgaveComposite/Test.java
+++ L-14-Iterator-And-Composite-Patterns/src/OpgaveComposite/Test.java
@@ -1,5 +1,26 @@
 package OpgaveComposite;
 
+
 public class Test {
+
+    public static void main(String[] args) {
+
+        //Ikke færdiggjort
+
+
+        Rectangle r1 = new Rectangle("R1", 1, 2);
+        Rectangle r2 = new Rectangle("R2", 2, 4);
+        Rectangle r3 = new Rectangle("R3", 4, 8);
+
+        Elipse e1 = new Elipse("E1",1,2);
+        Elipse e2 = new Elipse("E2",4,8);
+        Elipse e3 = new Elipse("E3",12,24);
+
+        Triangle t1 = new Triangle("T1", 3, 4, 5);
+        Triangle t2 = new Triangle("T2", 6, 8, 10);
+        Triangle t3 = new Triangle("T3", 9, 12, 15);
+
+
+    }
 
 }
